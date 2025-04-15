b4b0812200d2abd3249d2fdbcc62f57a7b2cf4dc
Marco Carletti <mcarlett@redhat.com>
2025-04-15 13:01:27 +0200

    Adds OCP OAuth authentication on Kafka example

---
+++ examples/ocp/kafka-oauth/camel-spring-boot/kafka-oauth/src/main/java/org/example/project/KafkaOauthRestRouter.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example.project;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestParamType;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+/**
+ * The Camel router to configure the REST endpoints
+ */
+@Component
+public class KafkaOauthRestRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        rest("/api/kafka")
+                .produces(MediaType.TEXT_PLAIN_VALUE)
+                .consumes(MediaType.TEXT_PLAIN_VALUE)
+
+                .post()
+                    .routeId("rest-send-message")
+                    .param().type(RestParamType.query).name("id").dataType("int").required(true).endParam()
+                    .to("direct:kafka-producer");
+    }
+
+}
